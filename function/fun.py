e9de1a9440dd1c491f7d6efb512df297702d7a05
Omkar Padmane <125428693+OmkarPadmane@users.noreply.github.com>
2024-03-21 10:21:44 +0530

    Add files via upload

---
+++ function/fun.py
@@ -0,0 +1,9 @@
+
+def my_addition(number1,number2):
+    print("addition: ",number1 + number2)
+    print("square : ",number1 **2)
+    print("first number raised to number second number",number1 ** number2)
+    
+
+def my_subtraction(number1,number2):
+    print("subtraction: ",number1 - number2)
