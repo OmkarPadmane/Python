48045676b50b63ec3068169c63c0204a650d9a27
omkar_padmane <125428693+OmkarPadmane@users.noreply.github.com>
2024-03-31 17:20:54 +0530

    Add files via upload

---
+++ List_practice.py
@@ -0,0 +1,39 @@
+list_name = ["a","b",56,"fhg5757","c","d","a","f"]
+# print(list_name)
+
+# print(len(list_name))
+
+# print(list_name[-1])
+# print(list_name[:-1])
+
+# if 56 in list_name:
+#     print("yes")
+# else:
+#     print("no")
+
+# list_name[5] = ["abcde", "hfkjdh"]
+# print(list_name)
+
+# thislist = ["apple", "banana", "cherry"]
+# list_name.extend(thislist)
+
+# list_name.remove ("f")
+# print(list_name)
+
+# list_name.clear()
+# print(list_name)
+
+ 
+# for x in range(len(list_name)):
+#     print(x)
+
+
+
+
+
+
+
+
+
+
+
