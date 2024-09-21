1f45c3e752eb5a66e6f3f07a0fdeac5dd46ace1d
OmkarPadmane <omkarpadmane8801@gmail.com>
2024-09-21 17:16:01 +0530

    2 commit

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-
+'''
 print("hello")
 a = int(5) 
 b = int(6)
@@ -7,9 +7,23 @@ b = a - b
 a = a - b
 print("a: " ,a)
 print("b: " ,b)
-
+'''
 
 '''
 print(2+2/((2+2)+(2**2)))
 print(365*24)
 '''
+
+no1 = int(input("Enter 1 no: "))
+no2 = int(input("Enter 2 no: "))
+
+print("sum of a and b is ", no1 + no2 )
+
+'''
+a = 5
+b = 5
+print("sum of a and b is ", a + b)
+'''
+
+
+
